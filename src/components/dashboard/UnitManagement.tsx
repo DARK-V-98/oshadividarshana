cf916e7c9dfc84b6d56b29f200a35500e5c17859
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2026-01-03 08:53:06 +0000

    Try fixing this error: `Runtime TypeError: (0 , {imported module [projec

--- src/components/dashboard/UnitManagement.tsx
+++ src/components/dashboard/UnitManagement.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState } from "react";
-import { useForm } from "react";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { useFirestore } from "@/firebase";
@@ -267,5 +267,3 @@ export default function UnitManagement() {
      </div>
   );
 }
-
-    
