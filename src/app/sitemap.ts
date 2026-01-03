af6f6621a820286344efd2f790507e86422e0227
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2026-01-03 14:06:45 +0000

    also make sitemap.xml file dont genarate it using server just make the f

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,2 @@
-import { MetadataRoute } from 'next';
-import { moduleCategories } from '@/lib/data';
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = 'https://www.oshadividarshana.online';
-
-  // Static routes
-  const routes = [
-    '', 
-    '/order',
-  ].map((route) => ({
-    url: `${siteUrl}${route}`,
-    lastModified: new Date().toISOString(),
-  }));
-
-  // Dynamic course routes
-  const courseRoutes = moduleCategories.map((category) => ({
-    url: `${siteUrl}${category.href}`,
-    lastModified: new Date().toISOString(),
-  }));
-
-  return [...routes, ...courseRoutes];
-}
+// This file is no longer in use and can be deleted. 
+// A static sitemap.xml has been created in the /public directory.
