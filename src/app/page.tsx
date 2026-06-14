9d21168a23a91f6422b9e2884548c2cc3c1df981
esystemlk <esystemlk@gmail.com>
2026-06-14 23:43:45 +0530

    tr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ export default function Home() {
       <MarqueeBar />
       <PromoSection />
       <CategoriesSection />
+      <NailsSection />
       <PrintBindSection />
       <WhyChooseSection />
       <AboutSection />
       <HowItWorksSection />
-      <NailsSection />
       <TestimonialsSection />
       <ContactSection />
     </>
